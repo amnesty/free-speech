33b955ba60c62eca707e779ce9410b227db90ef9
elcapo <mail@carloscapote.com>
2014-05-11 19:52:11 +0200

    Removes Non-Translatable Text from __( Call

--- content.php
+++ content.php
@@ -21,7 +21,7 @@
 	</div><!-- .entry-summary -->
 	<?php else : ?>
 	<div class="entry-content">
-		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'amnistia' ) ); ?>
+		<?php the_content( __( 'Continue reading', 'amnistia' ) . ' <span class="meta-nav">&rarr;</span>' ); ?>
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . __( 'Pages:', 'amnistia' ),
