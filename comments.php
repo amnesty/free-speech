66e0c0c313009bb017374d99708d7e24f2d7f980
elcapo <mail@carloscapote.com>
2014-05-11 22:36:24 +0200

    Changes Translation Expression to Facilitate PoEditor work

--- comments.php
+++ comments.php
@@ -27,8 +27,9 @@ if ( post_password_required() ) {
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'amnistia' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+				printf( _n( __( 'One thought on &ldquo;%2$s&rdquo;', 'amnistia' ),
+                                            __( '%1$s thoughts on &ldquo;%2$s&rdquo;', 'amnistia' ), get_comments_number() ),
+					     number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
 			?>
 		</h2>
 
