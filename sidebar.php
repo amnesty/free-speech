5c3adef35de5a5fa489e3a34cd0f4a6f093aaf3b
elcapo <mail@carloscapote.com>
2014-05-11 17:44:49 +0200

    Checks if get_field_object() exists

--- sidebar.php
+++ sidebar.php
@@ -17,11 +17,32 @@
 	<a href="<?php the_field('pinterest', 'options');?>" class="perfiles perfil_pinterest" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/img/blank.png"></a>
 </div>
 <div class="logo_escuelas">
-<?php $fields = get_field_object('logo', 'options'); ?>
-<img src="<?php echo $fields['value']['url']; ?>" alt="<?php echo $fields['value']['alt']; ?>" />
+    <?php
+        $sidebar_logo_url = get_bloginfo( 'template_directory' ) . "/img/default_sidebar_logo.png";
+        $sidebar_logo_alt = get_bloginfo( 'name' );
+
+        if( function_exists( 'get_field_object' ) ) {
+            $fields = get_field_object( 'logo', 'options' );
+            if( array_key_exists( 'url', $fields['value'] ) ) {
+                $sidebar_logo_url = $fields['value']['url'];
+                $sidebar_logo_alt = $fields['value']['alt'];
+            }
+        }
+    ?>
+    <img src="<?php echo $sidebar_logo_url; ?>" alt="<?php echo $sidebar_logo_alt; ?>" />
 </div>
-<?php $fields2 = get_field_object('logo_fondo_menu', 'options'); ?>
-<nav id="site-navigation" class="main-navigation" role="navigation" style="background: url(<?php echo $fields2['value']['url']; ?>) no-repeat left center;">
+
+<?php
+    $sidebar_fondo_url = get_bloginfo( 'template_directory' ) . "/img/default_sidebar_fondo.png";
+
+    if( function_exists( 'get_field_object' ) ) {
+        $fields2 = get_field_object( 'logo_fondo_menu', 'options' );
+        if( array_key_exists( 'url', $fields2['value'] ) ) {
+            $sidebar_fondo_url = $fields['value']['url'];
+        }
+    }
+?>
+<nav id="site-navigation" class="main-navigation" role="navigation" style="background: url(<?php echo $sidebar_fondo_url; ?>) no-repeat left center;">
 	<div class="screen-reader-text skip-link">
 		<a href="#content" title="<?php esc_attr_e( 'Skip to content', 'amnistia' ); ?>"><?php _e( 'Skip to content', 'amnistia' ); ?></a>
 	</div>
